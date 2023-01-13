56856e257f28fa6b296c1f44aec260261c510dbf
Jacob Nesbitt <jjnesbitt2@gmail.com>
2023-01-13 13:38:52 -0500

    Fix CLI installation and workflow

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(
-        entry_points={
-            "console_scripts": [
-                "zarrsum=zarr_checksum.cli:cli",
-            ]
-        }
-    )
+    setuptools.setup()
